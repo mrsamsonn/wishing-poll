699384251cb347a4a059d6748e1f23dbebc143b7
mrsamsonn <andrewsj.ahs@gmail.com>
2024-08-22 07:28:54 -0700

    update settings

--- wishing_poll/settings.py
+++ wishing_poll/settings.py
@@ -22,11 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = '32132'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS = []
 
 
 # Application definition
