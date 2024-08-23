e95fba4bb82790845bac5abe94079a2c80c4e6c3
mrsamsonn <andrewsj.ahs@gmail.com>
2024-08-23 09:17:24 -0700

    update debug settings

--- wishing_poll/settings.py
+++ wishing_poll/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECRET_KEY = '32132'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG","False").lower() == "true"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 # ALLOWED_HOSTS = ['127.0.0.1']
